b7dd26d6d8c8b2cab0cc5b060a3fc4d5cadee174
Harvey Kadyanji <harvey.kadyax@gmail.com>
2022-10-27 05:37:48 +0300

    remove env traces

--- helpers/config.ts
+++ helpers/config.ts
@@ -1,6 +1,3 @@
 import * as flags from 'https://deno.land/std/flags/mod.ts';
-import { config } from 'https://deno.land/x/dotenv/mod.ts';
 
-const env = config();
-
-export const PORT = flags.parse(Deno.args).port ?? Number(env.PORT);
+export const PORT = flags.parse(Deno.args).port ?? Number(Deno.env.get('PORT'));
