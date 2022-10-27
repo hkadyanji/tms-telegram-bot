2c6337f2603bbb13e988e8224c345ab33b658974
Harvey Kadyanji <harvey.kadyax@gmail.com>
2022-10-27 16:09:36 +0300

    clean up

--- controllers/vehicle.ts
+++ controllers/vehicle.ts
@@ -91,11 +91,15 @@ const handleIncoming = async (ctx: Context) => {
     },
   };
 
-  await fetch(url, {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: { 'Content-Type': 'application/json' },
-  });
+  try {
+    await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch(e) {
+    console.log('ee ', e);
+  }
 
   ctx.response.status = 200;
 }
